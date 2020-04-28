23feb0553f463f759aaa346817ed2af0f661efb6
David Chun <cdavid0@vt.edu>
2020-04-28 08:40:38 -0400

    Updates

---
+++ precache-manifest.76c945a32e4f2d97045aa9f5b4ee9fdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511258df3995294dd1361b764764d8b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8fbe585adeab0e669604",
+    "url": "/monsters-rolodex/static/css/main.bbb9d9c9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbe585adeab0e669604",
+    "url": "/monsters-rolodex/static/js/main.9038e633.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
